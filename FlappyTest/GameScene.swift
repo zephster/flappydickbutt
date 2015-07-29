ed395b9b59f0d16277d475a8c784fb74c6596ad4
Brandon <antizeph@gmail.com>
2015-07-28 20:00:11 -0400

    hittin the pipes

--- FlappyTest/GameScene.swift
+++ FlappyTest/GameScene.swift
@@ -11,9 +11,11 @@ import SpriteKit
 class GameScene: SKScene, SKPhysicsContactDelegate
 {
     // just define the textures, they are added to SKSpriteNodes when the object is created
-    let heroTexture   = SKTexture(imageNamed: "dickbutt")
-    let skyTexture    = SKTexture(imageNamed: "sky")
-    let groundTexture = SKTexture(imageNamed: "ground")
+    let heroTexture     = SKTexture(imageNamed: "dickbutt")
+    let skyTexture      = SKTexture(imageNamed: "sky")
+    let groundTexture   = SKTexture(imageNamed: "ground")
+    let pipeUpTexture   = SKTexture(imageNamed: "PipeUp")
+    let pipeDownTexture = SKTexture(imageNamed: "PipeDown")
 
     let skyColor = SKColor(red: 0, green: 191, blue: 255, alpha: 1)
 
@@ -27,6 +29,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     let levelCat: UInt32 = 1 << 2 // 4
     let scoreCat: UInt32 = 1 << 3 // 8
 
+    // gap between top and bottom pipes
+    let pipeGap:CGFloat = 150.0
+
+    // z-indexes of stuff
+    let groundZpos:CGFloat = 0
+    let skyZpos:CGFloat    = -1
+    let pipeZpos:CGFloat   = -1
+
     // dickbutt!
     var hero: SKSpriteNode!
 
@@ -35,6 +45,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate
 
     // ground area, physics collision
     var groundNode = SKNode()
+
+    // vertical area for pipes
+    var pipeNode = SKNode()
+
+
+    var pipeNodeAnimation: SKAction!
 
 
 
@@ -57,26 +73,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         self.hero = self.setupHero()
         self.setupGround()
         self.setupSkyLine()
+        self.setupPipes()
     }
 
 
+    // TODO: remove collision from pipe so it passes through, still want the event though
+    // cause when they touch, the game will stop anyway
+
+    // TODO: prevent dickbutt from going off-screen (top)
     func setupHero() -> SKSpriteNode
     {
-        let hero      = SKSpriteNode(texture: heroTexture)
+        let hero      = SKSpriteNode(texture: self.heroTexture)
         hero.position = CGPoint(x: self.frame.size.width * 0.33, y: self.frame.size.height * 0.75)
 
         // set the "hitbox" for physics interactions
-        hero.physicsBody = SKPhysicsBody(rectangleOfSize: hero.size)
+        // hero.physicsBody = SKPhysicsBody(rectangleOfSize: hero.size) // square hitbox
+        hero.physicsBody = SKPhysicsBody(texture: self.heroTexture, size: hero.size) // object-shape hitbox
         hero.physicsBody?.allowsRotation = false
 
         // set this value so when collission happens, i can test the "category" property to determine
         // what to do, depending on what object gets hit, defined below
         hero.physicsBody?.categoryBitMask = self.heroCat
 
-        // determines which objects hero is capable of colliding with
+        // determines which objects hero is capable of colliding and interacting with (bouncing off, etc)
         hero.physicsBody?.collisionBitMask = self.pipeCat | self.levelCat
 
-        // determines which objects send an event when collided with
+        // determines which objects send an event when collided with (without collisionBitMask, objects would pass through but still be detected)
         hero.physicsBody?.contactTestBitMask = self.pipeCat | self.levelCat
 
         // add to scene
@@ -102,7 +124,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
 
         // define animations!
         // lower number = faster interval (duh)
-        let groundMoveTime = NSTimeInterval(10)
+        let groundMoveTime = NSTimeInterval(6.75)
 
         // move ground to the left (negative x-axis) on a timer, defined above
         let moveGround = SKAction.moveByX(-groundTexWidth * scale, y: 0, duration: groundMoveTime)
@@ -179,13 +201,137 @@ class GameScene: SKScene, SKPhysicsContactDelegate
             sprite.position  = CGPoint(x: spriteX, y: spriteY)
 
             // everything else resides on z-position 0
-            sprite.zPosition = -1
+            sprite.zPosition = -2
 
             sprite.runAction(skyAnimation)
             self.scrollNode.addChild(sprite)
         }
     }
 
+
+    func spawnPipes()
+    {
+        // node for the up and down pipe to reside
+        // create a column that the up/down pipes reside, and then set up hitboxes on each
+        let pipePair = SKNode()
+
+        // starting position just off-screen
+        pipePair.position = CGPoint(x: self.frame.size.width + self.pipeUpTexture.size().width, y: 0)
+
+        // z-position
+        pipePair.zPosition = self.pipeZpos
+
+        // pipe placement location, y-axis
+        let pipeY = CGFloat(self.frame.size.height / 4)
+
+        // random number for randomness. UInt32 -> Double -> CGFloat for it to work with .position
+        let random = CGFloat(Double(arc4random()))
+
+        // random = big number, mod by pipeY for a reasonable number, add pipeY for even-ness
+        let randomOffset = (random % pipeY) + pipeY
+
+
+        // set up pipes!
+        let topPipe = SKSpriteNode(texture: self.pipeDownTexture)
+        topPipe.setScale(2.0)
+
+        // set top pipe to top of screen + pipe gap space + random offset
+        topPipe.position = CGPoint(x: 0, y: topPipe.size.height + self.pipeGap + randomOffset)
+
+        // pipes are rectangular anyway, so use a rectangle hitbox
+        topPipe.physicsBody = SKPhysicsBody(rectangleOfSize: topPipe.size)
+
+        // disable physics forces (gravity pulls the pipe down, lol)
+        topPipe.physicsBody?.dynamic = false
+
+        // set category bitmask
+        topPipe.physicsBody?.categoryBitMask = self.pipeCat
+
+        // set contact bitmask to fire event with dickbutt
+        // TODO: test, if i omit this, the logical AND will = 0 if this and hero's bitmask are different, and event shouldn't fire
+        topPipe.physicsBody?.contactTestBitMask = self.heroCat
+
+        // add top pipe to pipe column
+        pipePair.addChild(topPipe)
+
+
+        // do it all again for the bottom pipe
+        let bottomPipe = SKSpriteNode(texture: self.pipeUpTexture)
+        bottomPipe.setScale(2.0)
+
+        // bottom pipe positions already on the bottom of the screen, so just give it the random offset
+        bottomPipe.position                        = CGPoint(x: 0, y: randomOffset)
+        bottomPipe.physicsBody                     = SKPhysicsBody(rectangleOfSize: bottomPipe.size)
+        bottomPipe.physicsBody?.dynamic            = false
+        bottomPipe.physicsBody?.categoryBitMask    = self.pipeCat
+        bottomPipe.physicsBody?.contactTestBitMask = self.heroCat
+        pipePair.addChild(bottomPipe)
+
+        pipePair.runAction(self.pipeNodeAnimation)
+        self.pipeNode.addChild(pipePair)
+    }
+
+
+    // this is called once, spawnPipes is called at an interval
+    // this creates 2 actions:
+    //      one that spawns and delays (repeated forever)
+    //      one that moves the pipe from right to left off-screen, then removes it
+    func setupPipes()
+    {
+        // first, set up the pipe animations themselves
+        // set to a class property so it can be used by the pipe objects,
+        // it's only configured (once) here, instead of every spawn (that'd be dumb)
+        self.pipeNodeAnimation = self.setupPipeNodeAnimation()
+
+        // now, set up the constant spawning of the pipes
+        // time between pipes
+        let pipeInterval = NSTimeInterval(2)
+        let delay        = SKAction.waitForDuration(pipeInterval)
+
+        // the action of spawning the pipe
+        let spawn = SKAction.runBlock({
+            self.spawnPipes()
+        })
+
+        // sequence of: spawn a pipe, then delay (until it gets called again)
+        let pipeSequence = SKAction.sequence([spawn, delay])
+
+        // do the above forever
+        let pipeSpawn = SKAction.repeatActionForever(pipeSequence)
+
+        // start the spawning!
+        self.runAction(pipeSpawn)
+
+        // finally, add the pipeNode, which will hold all the pipe objects
+        self.addChild(self.pipeNode)
+    }
+
+
+    private func setupPipeNodeAnimation() -> SKAction
+    {
+        // distance to move the pipe all the way to the left (frame width) + it's own size, so it's all off-screen
+        let distanceToMove = CGFloat(self.frame.size.width + self.pipeUpTexture.size().width)
+
+        // how fast it moves
+        // set to 10 to match ground's 6.5 (texture width causes the difference)
+        let pipeMoveTime = NSTimeInterval(7.50)
+
+        // actions to move the pipe, and remove the pipe (after it goes off-screen)
+        let movePipes   = SKAction.moveByX(-distanceToMove, y:0, duration:pipeMoveTime)
+        let removePipes = SKAction.removeFromParent()
+
+        // sequence of moving, removing pipes.
+        return SKAction.sequence([movePipes, removePipes])
+    }
+
+
+
+
+    func didBeginContact(contact: SKPhysicsContact)
+    {
+        println("contact!")
+        println(contact)
+    }
 
 
     private func log(message: String)
@@ -202,7 +348,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         self.hero.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
 
         // then apply a force upwards
-        self.hero.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 50))
+        self.hero.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 25))
     }
    
     override func update(currentTime: CFTimeInterval) {
